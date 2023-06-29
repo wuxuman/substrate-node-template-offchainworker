75794cbebc517f4a93da97c7f7dcfb7ad0df3a48
wuxuman <wxm1985betty@163.com>
2023-06-29 16:06:16 +0800

    add template6, get http GithubInfo

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -20,8 +20,8 @@ use sp_io::offchain_index;
 use core::str;
 use codec::{Encode, Decode};
 use frame_support::inherent::Vec;
-use serde::{Deserialize};
-use sp_runtime::offchain::storage::StorageValueRef;
+use serde::{Deserialize, Deserializer};
+use sp_runtime::offchain::{storage::StorageValueRef,http,Duration};
 
 
 
@@ -35,6 +35,37 @@ pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use frame_support::inherent::Vec;
+
+	#[derive(Deserialize, Encode, Decode)]
+    struct GithubInfo {
+        #[serde(deserialize_with = "de_string_to_bytes")]
+        login: Vec<u8>,
+        #[serde(deserialize_with = "de_string_to_bytes")]
+        blog: Vec<u8>,
+        public_repos: u32,
+    }
+
+    pub fn de_string_to_bytes<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
+        where
+        D: Deserializer<'de>,
+        {
+            let s: &str = Deserialize::deserialize(de)?;
+            Ok(s.as_bytes().to_vec())
+        }
+
+    use core::{convert::TryInto, fmt};
+    impl fmt::Debug for GithubInfo {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "{{ login: {}, blog: {}, public_repos: {} }}",
+                sp_std::str::from_utf8(&self.login).map_err(|_| fmt::Error)?,
+                sp_std::str::from_utf8(&self.blog).map_err(|_| fmt::Error)?,
+                &self.public_repos
+                )
+        }
+    }
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
@@ -144,6 +175,7 @@ pub mod pallet {
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		fn offchain_worker(block_number: T::BlockNumber) {
+			//get data from offchain storage
 			let key = Self::derived_key(block_number);
 			log::info!("=====> offchain_worker:block_number is {:?}.",&block_number);
 			let storage_ref = StorageValueRef::persistent(&key);
@@ -155,6 +187,18 @@ pub mod pallet {
 			} else {
 				log::info!("=====>offchain_worker:Nothing reading from local storage.");
 			}
+
+
+			//get http GithubInfo
+			log::info!("OCW ==> Hello World from offchain workers!: {:?}", block_number);
+
+            if let Ok(info) = Self::fetch_github_info() {
+                log::info!("OCW ==> Github Info: {:?}", info);
+            } else {
+                log::info!("OCW ==> Error while fetch github info!");
+            }
+
+         	log::info!("OCW ==> Leave from offchain workers!: {:?}", block_number);
 		}
 	}
 
@@ -170,6 +214,37 @@ pub mod pallet {
                     .collect::<Vec<u8>>()
             })
         }
+
+
+		fn fetch_github_info() -> Result<GithubInfo, http::Error> {
+            // prepare for send request
+            let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(8_000));
+            let request =
+                http::Request::get("https://api.github.com/orgs/substrate-developer-hub");
+            let pending = request
+                .add_header("User-Agent", "Substrate-Offchain-Worker")
+                .deadline(deadline).send().map_err(|_| http::Error::IoError)?;
+            let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
+            if response.code != 200 {
+                log::warn!("Unexpected status code: {}", response.code);
+                return Err(http::Error::Unknown)
+            }
+            let body = response.body().collect::<Vec<u8>>();
+            let body_str = sp_std::str::from_utf8(&body).map_err(|_| {
+                log::warn!("No UTF8 body");
+                http::Error::Unknown
+            })?;
+
+            // parse the response str
+            let gh_info: GithubInfo =
+                serde_json::from_str(body_str).map_err(|_| http::Error::Unknown)?;
+
+            Ok(gh_info)
+        }
+
+
+
+
 	  }
 
 }
